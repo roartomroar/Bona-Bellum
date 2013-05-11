75b45871d5a2f601c852f3bc9e38d9f02358737a
15086 <15086@wst_ir_ss210.wst.esu3.k12.ne.us>
2013-05-11 10:21:04 -0500

    Unit Test not running correctly

--- test/animation/TanksMapLoaderTest.java
+++ test/animation/TanksMapLoaderTest.java
@@ -1,13 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package animation;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
