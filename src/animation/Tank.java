ae25d41b8818f6bebfda4608da9313b94e69cf4c
23324 <23324@wst_ir_ss209.wst.esu3.k12.ne.us>
2013-05-14 11:30:48 -0500

    Added variables to tank

--- src/animation/Tank.java
+++ src/animation/Tank.java
@@ -6,11 +6,15 @@ package animation;
  */
 public class Tank {
 
-    private int x, y;
+    private int x, y, hp, dolards;
+    private double fuel;
 
     public Tank(int x, int y) {
         this.x = x;
         this.y = y;
+        this.hp = 100;
+        this.fuel = 50;
+        this.dolards = 0;
     }
     
     static TanksMap.Ground[][] map;
