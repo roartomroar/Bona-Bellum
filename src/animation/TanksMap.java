e0a848e99b3e033322fd8f91547ff5dd1f40f72a
thomas <thomas@roartomroar>
2013-05-13 23:10:00 -0500

    Almost finished with the map class.

--- src/animation/TanksMap.java
+++ src/animation/TanksMap.java
@@ -1,9 +1,13 @@
 package animation;
 
+import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
+import javax.imageio.ImageIO;
 
 public class TanksMap
 {
@@ -24,20 +28,27 @@ public class TanksMap
     }
     
     private Ground[][] map;
-    private int x;
-    private int y;
     
-    public static final BufferedImage Grass;
-    public static final BufferedImage Dirt;
-    public static final BufferedImage Sand;
-    public static final BufferedImage Water;
-    public static final BufferedImage Wall;
+    private static final int TileSize = 50;
+    public static BufferedImage Grass;
+    public static BufferedImage Dirt;
+    public static BufferedImage Sand;
+    public static BufferedImage Water;
+    public static BufferedImage Wall;
     
     public TanksMap(Level level)
     {
-	if (Grass == null)
-	    
-	
+        try {
+            Grass = ImageIO.read(new File("Grass.png"));
+            Wall = ImageIO.read(new File("Wall.png"));
+            Water = ImageIO.read(new File("Water.png"));
+            Dirt = ImageIO.read(new File("Dirt.png"));
+        } catch (IOException ex) {
+            System.err.println("Could not load images!\n");
+            System.exit(-1);
+        }
+
+        
 	String fName = "";
 	
 	switch (level)
@@ -62,16 +73,31 @@ public class TanksMap
 		IOException e = new IOException("Invalid file format.");
 		throw e;
 	    }
-	    
+           
 	    int width = fileReader.nextInt();
 	    int height = fileReader.nextInt();
-	    
+
+            tanks = new ArrayList<Tank>();
+            /* Scanning for player positions, assuming correct format  */
+            while (fileReader.hasNextInt()) {
+                int tankX = fileReader.nextInt(),
+                        tankY = fileReader.nextInt();
+                tanks.add(new Tank(tankX, tankY));
+            } 
+            
+
 	    map = new Ground[width][height];
 	    
+            /* Literally just skipping lines in the file until I get to 
+             * where I need to be*/
+            while (fileReader.nextLine().equals("[VISUAL]"));
+            
 	    for (int x = 0; x < width; x++)
 	    {
 		for (int y = 0; y < height; y++)
 		{
+                    if (!fileReader.hasNextInt())
+                        break;
 		    int spot = fileReader.nextInt();
 		    switch (spot)
 		    {
@@ -93,9 +119,52 @@ public class TanksMap
 	    
 	} catch (IOException e)
 	{
-	    
+	    System.out.printf("ERROR: Could not load level %s due to %s",
+                    fName, e.getMessage());
+            map = null;
+            Wall = null;
+            Water = null;
+            Dirt = null;
+            Grass = null;
+            /* Exit from an error I don't feel like recovering from */
+            System.exit(1);
+
 	}
     }
     
+    public void DrawMap (Graphics2D gr) {
+        for (int x = 0; x < map.length; ++x) {
+            for (int y = 0; y < map[x].length; ++y) {
+                int xFinal = (x * TileSize) + TileSize;
+                int yFinal = (y * TileSize) + TileSize;
+                
+                switch (map[x][y]) {
+                    case GRASS:
+                        gr.drawImage(Grass, x  * TileSize, y * TileSize,
+                                xFinal, yFinal, 0, 0, 50, 50, null);
+                    case DIRT:
+                        gr.drawImage(Dirt, x  * TileSize, y * TileSize,
+                                xFinal, yFinal, 0, 0, 50, 50, null);
+                    case WATER:
+                        gr.drawImage(Water, x  * TileSize, y * TileSize,
+                                xFinal, yFinal, 0, 0, 50, 50, null);
+                    case WALL:
+                        gr.drawImage(Wall, x  * TileSize, y * TileSize,
+                                xFinal, yFinal, 0, 0, 50, 50, null);
+
+                }
+            }
+        }
+    }
+
+    public Ground[][] getMap() {
+        return map;
+    }
+
+    public ArrayList<Tank> getTanks() {
+        return tanks;
+    }
     
+    
+    ArrayList<Tank> tanks;
 }
