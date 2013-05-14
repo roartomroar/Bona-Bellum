10d6b366c7ce36b5e7d22640c40dc281f73a71c2
thomas <thomas@roartomroar>
2013-05-13 23:09:16 -0500

    Added skeleton Tank class

---
+++ src/animation/Tank.java
@@ -0,0 +1,18 @@
+package animation;
+
+
+/*
+ * Really basic as of yet
+ */
+public class Tank {
+
+    private int x, y;
+
+    public Tank(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    static TanksMap.Ground[][] map;
+    
+}
