d454a0b631c53ac119c92cf38c1de157a0b6c52d
15086 <15086@wst_ir_ss210.wst.esu3.k12.ne.us>
2013-05-15 08:03:05 -0500

    Added events

--- src/animation/TankScreen.java
+++ src/animation/TankScreen.java
@@ -1,6 +1,9 @@
 package animation;
 
 import java.awt.Graphics2D;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
 public class TankScreen implements Screen {
@@ -10,11 +13,25 @@ public class TankScreen implements Screen {
         
         map = new TanksMap(TanksMap.Level.EASY);
         tanks = map.getTanks();
+        CurrentTank = tanks.get(0);
     }
 
     @Override
     public void update() {
-        /* TODO: Implement the updating */
+        InputEvent ev = theFrame.getNextEvent();
+        
+        if (ev == null)
+            return;
+        
+        if (ev instanceof MouseEvent) {
+            /* TODO: Tell the tank to either attack or
+             * move to the location */
+            System.out.println("Pressed the mouse");
+        } else if ( ev instanceof KeyEvent) {
+            /*
+             * TODO: Implement junk like pausing, etc.
+             */
+        }
     }
 
     @Override
@@ -30,4 +47,5 @@ public class TankScreen implements Screen {
     private AnimatedFrame theFrame;
     private TanksMap map;
     private ArrayList<Tank> tanks;
+    private Tank CurrentTank;
 }
